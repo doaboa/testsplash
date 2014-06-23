f9e9f2a080b466d502097416d65820eb25c608a1
Doa <doajafri@gmail.com>
2014-06-23 14:26:06 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var app = (function(document, $) {
+
+	'use strict';
+	var docElem = document.documentElement,
+
+		_userAgentInit = function() {
+			docElem.setAttribute('data-useragent', navigator.userAgent);
+		},
+		_init = function() {
+			$(document).foundation();
+			_userAgentInit();
+		};
+
+	return {
+		init: _init
+	};
+
+})(document, jQuery);
+
+(function() {
+
+	'use strict';
+	app.init();
+
+})();
+
+$(document).ready(function($) {
+	var scroll_pos = 0;
+            $(document).scroll(function() { 
+                scroll_pos = $(this).scrollTop();
+                if(scroll_pos > 10) {
+                    $("body").css('$percent-bg', '95%');
+                } else {
+                    $("body").css('$percent-bg', '15%');
+                }
+            });
+});
