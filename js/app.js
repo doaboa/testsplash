8e8ee772171f29438556ddccf408b58a4bf31a1d
Doa <doajafri@gmail.com>
2014-06-24 18:53:31 -0500

    fixed links

--- js/app.js
+++ js/app.js
@@ -26,12 +26,12 @@ var app = (function(document, $) {
 
 $(document).ready(function($) {
 	var scroll_pos = 0;
-            $(document).scroll(function() { 
-                scroll_pos = $(this).scrollTop();
-                if(scroll_pos > 10) {
-                    $("body").css('$percent-bg', '95%');
-                } else {
-                    $("body").css('$percent-bg', '15%');
-                }
-            });
+        $(document).scroll(function() { 
+            scroll_pos = $(this).scrollTop();
+            if(scroll_pos > 10) {
+                $("body").addClass("scrooll");
+            } else {
+                $("body").removeClass("scrooll");
+            }
+        });
 });
